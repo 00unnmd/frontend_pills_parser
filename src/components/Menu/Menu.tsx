8a65409f503381d57f9b939e75a33e1dde581113
00unnmd <zerozerounnamed@yahoo.com>
2025-06-10 10:46:19 +0400

    [PLPR-14] - Added app version comp. Fixed local date rendering.

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,14 @@
 import { Menu } from "react-admin";
 import { AppMenuProps } from "./types.ts";
+import { AppVersion } from "./AppVersion.tsx";
 
 const AppMenu = (props: AppMenuProps) => {
   return (
     <Menu {...props}>
       <Menu.DashboardItem />
       <Menu.ResourceItems />
+
+      <AppVersion />
     </Menu>
   );
 };
