5483fbe9387a5d25724e8dd7a6ebc9f9a0c29699
00unnmd <zerozerounnamed@yahoo.com>
2025-06-19 17:55:36 +0400

    [PLPR-19] - Fixed a little vertical scroll on all pages.

--- src/utils/themes.ts
+++ src/utils/themes.ts
@@ -1,7 +1,7 @@
 import { deepmerge } from "@mui/utils";
 import { bwDarkTheme, bwLightTheme } from "react-admin";
 
-const lightTheme = deepmerge(bwLightTheme, {
+const mergeSource = {
   components: {
     RaLogin: {
       styleOverrides: {
@@ -34,43 +34,17 @@ const lightTheme = deepmerge(bwLightTheme, {
         size: "small",
       },
     },
+    MuiDrawer: {
+      styleOverrides: {
+        root: {
+          height: "calc(100vh - 48px) !important",
+        },
+      },
+    },
   },
-});
+};
 
-const darkTheme = deepmerge(bwDarkTheme, {
-  components: {
-    RaLogin: {
-      styleOverrides: {
-        root: {
-          background:
-            "linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898",
-          backgroundBlendMode: "multiply,multiply",
-        },
-      },
-    },
-    RaFilterForm: {
-      styleOverrides: {
-        root: {
-          padding: "4px 0 4px 0",
-        },
-      },
-    },
-    MuiChip: {
-      styleOverrides: {
-        root: {
-          height: "20px",
-          "&.RaSelectArrayInput-chip": {
-            marginTop: 0,
-          },
-        },
-      },
-    },
-    MuiInputLabel: {
-      defaultProps: {
-        size: "small",
-      },
-    },
-  },
-});
+const lightTheme = deepmerge(bwLightTheme, mergeSource);
+const darkTheme = deepmerge(bwDarkTheme, mergeSource);
 
 export { lightTheme, darkTheme };
