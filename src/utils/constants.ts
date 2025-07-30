7ebf8856440ba547c2801da41507623b7ee38a1e
00unnmd <zerozerounnamed@yahoo.com>
2025-07-30 23:46:46 +0400

    [PLPR-24] - Deployed v 1.2 of pills-parser frontend.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,4 @@ export const API_URL: string = import.meta.env.PROD
   ? import.meta.env.VITE_API_URL_PROD
   : import.meta.env.VITE_API_URL_DEV;
 
-export const APP_VERSION: string = "v1.2";
+export const APP_VERSION: string = "v1.3";
